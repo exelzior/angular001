cb6977df5df6c5675642498e5ba3b5885506e25f
DESKTOP-0HGRJKC\SCORE-ID001 <SCORE-ID001@SCORE-ID001>
2017-04-18 16:29:01 -0500

    first commit

---
+++ angular-seed/app/view2/view2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute','ngMaterial'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', [function() { 
+  var vm = this;
+  vm.user = null;
+  vm.users = null;
+
+ 
+
+      vm.users =  vm.users  || [
+        { id: 1, name: 'Scooby Doo' },
+        { id: 2, name: 'Shaggy Rodgers' },
+        { id: 3, name: 'Fred Jones' },
+        { id: 4, name: 'Daphne Blake' },
+        { id: 5, name: 'Velma Dinkley' }
+      ];
+
+  
+  
+}]);
